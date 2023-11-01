f1414b31fbd0f1ee9fa12ea4e36c2e028b0e2c9a
Anuj Kumar Karmakar <anujkarmakar999@gmail.com>
2023-11-01 05:46:48 +0530

    Updated Logic

--- test/script.js
+++ test/script.js
@@ -145,28 +145,3 @@ signInButton.addEventListener("click", (event) => {
 //             // Do something else with the error object
 //         });
 // });
-
-auth.onAuthStateChanged(user => {
-    if (user) {
-        // The user object has basic properties such as display name, email, etc.
-        const displayName = user.displayName;
-        const email = user.email;
-        const emailVerified = user.emailVerified;
-    
-        // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getToken() instead.
-        const uid = user.uid;
-    
-        document.getElementById("userInfo").innerHTML =
-            '<p>Name: ' + displayName + '</p>' +
-            '<p>Email: ' + email + '</p>' +
-            '<p>Email Verified: ' + emailVerified + '</p>';
-
-        alert("Yes It is retrieved");
-    }
-    else {
-        // No user is signed in.
-        document.getElementById('userInfo').innerHTML = 'No user is signed in.';
-    }
-});
