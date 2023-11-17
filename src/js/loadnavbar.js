5f5106d8332e3aa98496cdd481dc43a2a0624f4d
Anuj Kumar Karmakar <anujkarmakar999@gmail.com>
2023-11-17 20:30:21 +0530

    Updated Navbar

--- src/js/loadnavbar.js
+++ src/js/loadnavbar.js
@@ -1,36 +1,36 @@
 document.getElementById("nav-bar").innerHTML = `
-<div class="navbar-custom" id="navbar-custom">
-            <div class="navbar-custom-left-item">
-                <a href="#"><img src="/images/lawsy-logo.png" style="height: 100px; width: auto;" alt="lawsy logo"></a>
-            </div>
-            <div class="navbar-custom-center-item">
-                <ul>
-                    <li>
-                        <a href="/src/html/index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="/src/html/index.html#faqs">FAQs</a>
-                    </li>
-                    <li>
-                        <a href="/src/html/team.html">About Us</a>
-                    </li>
-                    <li>
-                        <a href="/src/html/contact.html">Get in touch</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="navbar-custom-right-item">
-                <ul>
-                    <li>
-                        <a href="/src/html/login.html"><button type="button"
-                                class="login-button">LOGIN</button></a>
-                    </li>
-                    <li>
-                        <a href="/src/html/register.html"><button type="button"
-                                class="signup-button">REGISTER</button></a>
-                    </li>
-                </ul>
-            </div>
-    
+    <div class="navbar-custom" id="navbar-custom">
+        <div class="navbar-custom-left-item">
+            <a href="/src/html/index.html"><img src="/images/lawsy-logo.png" style="height: 100px; width: auto;" alt="lawsy logo"></a>
         </div>
+        <div class="navbar-custom-center-item">
+            <ul>
+                <li>
+                    <a href="/src/html/index.html">Home</a>
+                </li>
+                <li>
+                    <a href="/src/html/index.html#faqs">FAQs</a>
+                </li>
+                <li>
+                    <a href="/src/html/team.html">About Us</a>
+                </li>
+                <li>
+                    <a href="/src/html/contact.html">Get in touch</a>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-custom-right-item">
+            <ul>
+                <li>
+                    <a href="/src/html/login.html"><button type="button"
+                            class="login-button">LOGIN</button></a>
+                </li>
+                <li>
+                    <a href="/src/html/register.html"><button type="button"
+                            class="signup-button">REGISTER</button></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
 `;
