efc9f85461939d44fc465fb3bd6580917bd5f6f3
Anuj Kumar Karmakar <anujkarmakar999@gmail.com>
2023-11-17 14:13:43 +0530

    Implemneted Navbar

---
+++ src/js/loadnavbar.js
@@ -0,0 +1,37 @@
+document.getElementById("nav-bar").innerHTML = `
+<div class="navbar-custom" id="navbar-custom">
+            <div class="navbar-custom-left-item">
+                <a class="navbar-brand" href="#"><img src="/images/lawsy-logo.png" height="100px" alt="lawsy logo"></a>
+                <!-- <a class="navbar-brand" href="#"><img src="/images/logo.png" width="100px"  alt="lawsy text logo"></a> -->
+            </div>
+            <div class="navbar-custom-center-item">
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        FAQs
+                    </li>
+                    <li>
+                        About Us
+                    </li>
+                    <li>
+                        Get in touch
+                    </li>
+                </ul>
+            </div>
+            <div class="navbar-custom-right-item">
+                <ul>
+                    <li>
+                        <a href="/src/html/login.html"><button type="button"
+                                class="login-button">LOGIN</button></a>
+                    </li>
+                    <li>
+                        <a href="/src/html/register.html"><button type="button"
+                                class="signup-button">REGISTER</button></a>
+                    </li>
+                </ul>
+            </div>
+    
+        </div>
+`;
